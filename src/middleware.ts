b619a65734def171381dd0dd08d75797cfed455f
faishalam <faishalabdulmajid@gmail.com>
2025-09-30 14:40:01 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,7 +25,7 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
-  if (pathname.startsWith('/login')) {
+  if (pathname.startsWith('/login') || pathname.startsWith('/activation')) {
     if (!token) {
       return NextResponse.next();
     }
